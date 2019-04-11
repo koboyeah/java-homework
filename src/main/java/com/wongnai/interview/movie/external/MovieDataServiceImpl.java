77bdc621a591faa2ec887e461fb9354473fefcad
fadeef <deef.narak@gmail.com>
2019-04-12 02:10:17 +0700

    deef - 0.8.0 - ข้อแรก

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,21 @@
 package com.wongnai.interview.movie.external;
 
+import net.minidev.json.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -19,11 +30,59 @@ public class MovieDataServiceImpl implements MovieDataService {
 
 	@Override
 	public MoviesResponse fetchAll() {
-		//TODO:
-		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
-		// Please noted that you must only read data remotely and only from given source,
-		// do not download and use local file or put the file anywhere else.
+		MoviesResponse movieJsonArray = new MoviesResponse();
+		try {
+			JSONArray jsonArray = readJsonFromUrl(MOVIE_DATA_URL);
+			for (int i = 0; i < jsonArray.length(); i++)
+			{
+				JSONObject subJsonObj = jsonArray.getJSONObject(i);
+				MovieData movieData = new MovieData();
+				movieData.setTitle(subJsonObj.getString("title"));
+				movieData.setYear(subJsonObj.getInt("year"));
+				if (subJsonObj.has("cast")) {
+					JSONArray subJsonArray = subJsonObj.getJSONArray("cast");
+					List<String> list = new ArrayList<>();
+					for(int j=0; j < subJsonArray.length(); j++) {
+						list.add(subJsonArray.getString(j));
+					}
+					movieData.setCast(list);
+				}
+				if (subJsonObj.has("genres")) {
+					JSONArray subJsonArray = subJsonObj.getJSONArray("genres");
+					List<String> list = new ArrayList<>();
+					for(int j=0; j < subJsonArray.length(); j++) {
+						list.add(subJsonArray.getString(j));
+					}
+					movieData.setGenres(list);
+				}
+				movieJsonArray.add(movieData);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return movieJsonArray;
+	}
 
-		return null;
+	public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			return  new JSONArray(jsonText);
+		} finally {
+			is.close();
+		}
+	}
+
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
 	}
 }
